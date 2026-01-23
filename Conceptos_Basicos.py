b82e4f8b2e30d216d3ad5a11329b649c3fb3a453
SantiagoJoyaDev <santiagojoyab@outlook.com>
2026-01-23 19:00:08 +0100

    Conceptos basicos

---
+++ Conceptos_Basicos.py
@@ -0,0 +1,7 @@
+print("Conceptos basicos de Docker")
+
+# Docker es una plataforma que permite crear, desplegar y ejecutar aplicaciones en contenedores.
+# Un contenedor es una unidad estandarizada de software que empaqueta el código y todas sus dependencias para que la aplicación se ejecute de manera rápida y confiable en cualquier entorno.
+# Docker file: es un archivo de texto que contiene una serie de instrucciones para construir una imagen de Docker.
+# imagen: Es un archivo o una plantilla que contiene todo lo necesario para ejecutar una aplicación, incluyendo el código, las bibliotecas, las dependencias y el sistema operativo.
+# Docker Hub: es un servicio de registro de imágenes de Docker en la nube que permite a los usuarios almacenar, compartir y distribuir imágenes de Docker.
